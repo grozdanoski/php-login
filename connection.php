2cf9678c79c8dd06bb6d64977d540593400a7b3b
teomk <teo.grozdanoski@gmail.com>
2016-07-04 09:17:52 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ $dbCon = mysqli_connect("localhost", "root", "123456", "learnlearn");
 
 
 if (mysqli_connect_errno()) {
-    echo "Failed to connect: " , mysqli_connect_error();
+    echo "Konekcijata e neuspesna: " , mysqli_connect_error();
 }
 
 ?>
